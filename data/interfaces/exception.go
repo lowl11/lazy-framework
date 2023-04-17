5ea207c0c86aaf87e5fd3d22eadfed3ebc83fa2b
lazy_owl <slimsly11@gmail.com>
2023-04-17 17:56:43 +0600

    exceptions fix (with() method)

--- data/interfaces/exception.go
+++ data/interfaces/exception.go
@@ -1,8 +1,11 @@
 package interfaces
 
+import "github.com/lowl11/lazy-framework/data/domain"
+
 type IException interface {
 	ToString() string
 	ToError() error
 	Business() string
 	Tech() string
+	With(err error) *domain.Exception
 }
