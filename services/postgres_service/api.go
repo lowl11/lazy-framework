b99c80d82ec2d34b5aa084ed1b8b570143df23b2
lazy_owl <slimsly11@gmail.com>
2023-05-08 16:21:32 +0600

    postgres service gives connection pool & just connection

--- services/postgres_service/api.go
+++ services/postgres_service/api.go
@@ -1,6 +1,7 @@
 package postgres_service
 
 import (
+	"context"
 	"github.com/jmoiron/sqlx"
 	"github.com/lowl11/lazy-framework/log"
 	"time"
@@ -8,22 +9,48 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func (service *Service) Connect() (*sqlx.DB, error) {
-	// подключение к Postgres
-	connection, err := sqlx.Open("postgres", service.connectionString)
+func (service *Service) ConnectionPool() (*sqlx.DB, error) {
+	// connection pool for Postgres
+	connectionPool, err := sqlx.Open("postgres", service.connectionString)
 	if err != nil {
 		return nil, err
 	}
 
+	// setting connection pool configurations
+	connectionPool.SetMaxOpenConns(service.maxConnections)
+	connectionPool.SetMaxIdleConns(service.maxConnections)
+	connectionPool.SetConnMaxIdleTime(time.Duration(service.maxLifetime) * time.Minute)
+
+	// ping database
+	log.Info("Ping Postgres database connection pool...")
+	if err = connectionPool.Ping(); err != nil {
+		return nil, err
+	}
+	log.Info("Ping Postgres database connection pool done!")
+
+	return connectionPool, nil
+}
+
+func (service *Service) Connection() (*sqlx.DB, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	// connection for Postgres
+	connection, err := sqlx.ConnectContext(ctx, "postgres", service.connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	// setting connection configurations
 	connection.SetMaxOpenConns(service.maxConnections)
 	connection.SetMaxIdleConns(service.maxConnections)
 	connection.SetConnMaxIdleTime(time.Duration(service.maxLifetime) * time.Minute)
 
-	log.Info("Ping Postgres database...")
+	log.Info("Ping Postgres database connection...")
 	if err = connection.Ping(); err != nil {
 		return nil, err
 	}
-	log.Info("Ping Postgres database done!")
+	log.Info("Ping Postgres database connection done!")
 
 	return connection, nil
 }
