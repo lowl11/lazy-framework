250ce43808bfffbc55090a041761246ee578992f
lazy_owl <slimsly11@gmail.com>
2023-05-09 16:50:03 +0600

    inner message add omitempty

--- data/domain/rest.go
+++ data/domain/rest.go
@@ -2,7 +2,7 @@ package domain
 
 type Response struct {
 	Status       string `json:"status"`
-	InnerMessage string `json:"inner_message"`
+	InnerMessage string `json:"inner_message,omitempty"`
 	Message      string `json:"message"`
 	Body         any    `json:"body,omitempty"`
 }
