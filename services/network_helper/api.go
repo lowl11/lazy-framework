4bca7c701c5ab4900aff63f8238fc25ee4392322
lazy_owl <slimsly11@gmail.com>
2023-06-07 16:20:52 +0600

    add network helper with no_proxy function

---
+++ services/network_helper/api.go
@@ -0,0 +1,28 @@
+package network_helper
+
+import (
+	"github.com/lowl11/lazy-framework/log"
+	"os"
+	"strings"
+)
+
+func NoProxy(hosts ...string) {
+	if len(hosts) == 0 {
+		return
+	}
+
+	SetEnv("no_proxy", strings.Join(hosts, ","))
+}
+
+func SetEnv(key, value string) {
+	lowercase := strings.ToLower(key)
+	uppercase := strings.ToUpper(key)
+
+	if err := os.Setenv(lowercase, value); err != nil {
+		log.Error(err, "Set "+key+" env error")
+	}
+
+	if err := os.Setenv(uppercase, value); err != nil {
+		log.Error(err, "Set "+key+" env error")
+	}
+}
